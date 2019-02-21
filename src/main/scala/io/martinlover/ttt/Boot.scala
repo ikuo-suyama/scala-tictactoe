afc985076858b02a23ebacb5acfead96d648035e
Ikuo Suyama <ikuo-suyama@users.noreply.github.com>
2019-02-21 23:57:31 +0900

    再帰 by 末尾再帰モナド

--- src/main/scala/io/martinlover/ttt/Boot.scala
+++ src/main/scala/io/martinlover/ttt/Boot.scala
@@ -1,3 +1,4 @@
+import scalaz.{-\/, \/, \/-}
 import scalaz.effect.{IO, SafeApp}
 import scalaz.effect.IO._
 
@@ -6,9 +7,15 @@ object MyApp extends SafeApp {
   override def runc: IO[Unit] = myAppLogic
 
   def myAppLogic: IO[Unit] =
+    IO.tailrecM(io)(0)
+
+  def io(i: Int): IO[Int \/ Unit] =
     for {
       _ <- putStrLn("Hello! What is your name?")
       n <- readLn
       _ <- putStrLn("Hello, " + n + ", good to meet you!")
-    } yield ()
+    } yield {
+      if (i >= 3) \/-()
+      else -\/(i + 1)
+    }
 }
