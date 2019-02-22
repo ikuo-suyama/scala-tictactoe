dae58de5c2f8bec88041137d8cb43f932d567538
Ikuo Suyama <ikuo-suyama@users.noreply.github.com>
2019-02-22 22:50:46 +0900

    Gameを実装

--- src/main/scala/io/martinlover/ttt/model/Player.scala
+++ src/main/scala/io/martinlover/ttt/model/Player.scala
@@ -4,3 +4,10 @@ sealed abstract class Player(val id: Int, val displayName: String)
 
 case object Black extends Player(1, "☓")
 case object White extends Player(2, "○")
+
+object Player {
+  def switch(p: Player): Player = p match {
+    case Black => White
+    case White => Black
+  }
+}
--- src/main/scala/io/martinlover/ttt/usecase/Game.scala
+++ src/main/scala/io/martinlover/ttt/usecase/Game.scala
@@ -1,11 +1,22 @@
 package io.martinlover.ttt.usecase
 
 import io.martinlover.ttt.model.Board.Point
+import io.martinlover.ttt.model.{Board, Player}
 
 trait Game {
   def drop(s: Status, point: Point): Result
 }
 
 class TicTokToe extends Game {
-  override def drop(s: Status, point: Point): Result = Finish
+
+  override def drop(s: Status, point: Point): Result =
+    (for {
+      _        <- checkReasonableMove(s.board, point)
+      newBoard <- move(s.board,                point)
+      _        <- checkGameOver(newBoard,      s.turn)
+    } yield Continue(Status(newBoard, Player.switch(s.turn)))) fold (identity, identity)
+
+  protected def checkReasonableMove(board: Board, point: Point): Either[Result, Unit] = Right(())
+  protected def move(board: Board, point: Point): Either[Result, Board]               = Right(board)
+  protected def checkGameOver(newBoard: Board, turn: Player): Either[Result, Unit]    = Right(())
 }
