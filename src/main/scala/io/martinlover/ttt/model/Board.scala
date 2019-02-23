4736a5a08519093b8963a4eb5bcd0bb83e1bbf6f
Ikuo Suyama <ikuo-suyama@users.noreply.github.com>
2019-02-23 22:11:46 +0900

    WIP

--- src/main/scala/io/martinlover/ttt/model/Board.scala
+++ src/main/scala/io/martinlover/ttt/model/Board.scala
@@ -3,6 +3,8 @@ package io.martinlover.ttt.model
 import io.martinlover.ttt.model.Board.Point
 
 class Board {
+  def isResonableMove(point: Point): Boolean = true
+
   def isFinished(): Boolean = false
 
   private val board: Map[Point, Int] = Map.empty
@@ -10,7 +12,7 @@ class Board {
 }
 
 object Board {
+  def drop(board: Board, point: Point): Board = board
 
   case class Point(i: Int, j: Int)
-
 }
--- src/main/scala/io/martinlover/ttt/usecase/Game.scala
+++ src/main/scala/io/martinlover/ttt/usecase/Game.scala
@@ -1,5 +1,6 @@
 package io.martinlover.ttt.usecase
 
+import com.sun.tools.classfile.Code_attribute.InvalidIndex
 import io.martinlover.ttt.model.Board.Point
 import io.martinlover.ttt.model.{Board, Player}
 
@@ -11,12 +12,17 @@ class TicTokToe extends Game {
 
   override def drop(s: Status, point: Point): Result =
     (for {
-      _        <- checkReasonableMove(s.board, point)
-      newBoard <- move(s.board,                point)
-      _        <- checkGameOver(newBoard,      s.turn)
+      _        <- checkReasonableMove(s,  point)
+      newBoard <- move(s.board,           point)
+      _        <- checkGameOver(newBoard, s.turn)
     } yield Continue(Status(newBoard, Player.switch(s.turn)))) fold (identity, identity)
 
-  protected def checkReasonableMove(board: Board, point: Point): Either[Result, Unit] = Right(())
-  protected def move(board: Board, point: Point): Either[Result, Board]               = Right(board)
-  protected def checkGameOver(newBoard: Board, turn: Player): Either[Result, Unit]    = Right(())
+  protected def checkReasonableMove(status: Status, point: Point): Either[Result, Unit] =
+    Either.cond(status.board.isResonableMove(point), (), InvalidInput(status))
+
+  protected def move(board: Board, point: Point): Either[Result, Board] =
+    Right(Board.drop(board, point))
+
+  protected def checkGameOver(newBoard: Board, turn: Player): Either[Result, Unit] =
+    Either.cond(newBoard.isFinished(), (), Finish)
 }
